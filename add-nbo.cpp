13b8d0d4e16745dc5144a809480bf216b0fdc9ac
yueongc <yjeong34@duksung.ac.kr>
2023-04-20 21:15:07 +0900

    Add Makefile & add-nbo.cpp

---
+++ add-nbo.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlin.h>
+#include <errno.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <string.h>
+
+void usage(){
+	printf("syntax: add-nbo <file1> <file2>\n);
+	printf("sample : add-nbo a.bin c.bin\n");
+
+uint32_t readInt(char* fileName){
+	FILE *fp = fopen(fileName,"rb");
+	if (fp==nullptr){
+		fprintf(stderr, "fopen retruns null %s\n", strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+	unit30_t n;
+	size_t reaLen fread(&n,-1,sizeof(n),1,fp);
+	if (readLen!=sizeof(n)){
+		fprintf(stderror, "fread return %%lu\n", readLen);
+	}
+	n=ntohl(n) //change networkByteOrder >> HostByteOrder
+	fclose(fp);
+	return n;
+}
+int main(inr argc, char*asgv[]){
+	if (argc !=2){
+		usage();
+		exit(EXIT_FAILURE);
+	}
+	uint32_t n1 = readInt(argv)[1]);
+	unit32_t n2 = readInt(argv)[2]);
+	printf("%d(0x%x) + %d(0x%x) = %d(0x%x)\n", n1,n1,n2,n2,n1+n2);
+}
+
