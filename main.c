eb771da0d3bfd10b89d949642d3c2bf1b355b674
yjeongc <yjeong34@duksung.ac.kr>
2024-07-25 00:26:01 +0900

    Edit main.c

--- main.c
+++ main.c
@@ -1,44 +1,45 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
+#include <stdint.h> //for uint32_t
 #include <netinet/in.h>
+#include <stdlib.h> //for exit()
 
-uint32_t read_number(const char *filename){
-	FILE* file;
-	file = fopen(filename, "rb");
+uint32_t open_read_file(const char *filename){
+	FILE *file;
+	file = fopen(filename, "r");
 	
 	if (file == NULL){
-		printf("Error: Unable to open file!");
+		printf("Error : Cannot Open File. Sorry!");
 		exit(1);
 	}
 
-	uint32_t number;
-	uint32_t size = fread(&number, sizeof(uint32_t), 1, file);
-	if(size != 1){
-		printf("Error reading from file!");
-		fclose(file);
+	uint32_t buffer;
+	size_t size;
+	size = fread(&buffer, sizeof(buffer), 1, file);
+
+	if(size<1){
+		printf("Error : Failed read 4bytes...");
 		exit(1);
 	}
 
 	fclose(file);
 
-	return ntohl(number);
+	return ntohl(buffer);
 }
 
 
-int main(int argc, char *argv[]){
-	if(argc != 3){
-		printf("Error : %s", argv[0]);
+int main(int argc, char* argv[]){
+	if (argc != 3){
+		printf("Sorry. Something is WRONG...");
 		return -1;
 	}
 
-	uint32_t number1 = read_number(argv[1]);
-	uint32_t number2 = read_number(argv[2]);
-	
-	uint32_t sum = number1 + number2;
+	uint32_t text[2];
+	text[0] = open_read_file(argv[1]);
+	text[1] = open_read_file(argv[2]);
 
-	printf("%u(0x%x) + %u(0x%x) = %u(0x%x)", number1, number1, number2, number2, sum, sum);
+	uint32_t sum = text[0] + text[1];
+
+	printf("%u(0x%x) + %u(0x%x) = %u(0x%x)\n", text[0], text[0], text[1], text[1], sum, sum);
 
 	return 0;
 }
-
